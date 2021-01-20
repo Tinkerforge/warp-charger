f8457b9044018ba647fa543f21defb434c824ea0
Matthias Bolte <matthias@tinkerforge.com>
2021-01-20 12:27:58 +0100

    label: Check for proper darkness

--- label/print-warp-label.py
+++ label/print-warp-label.py
@@ -104,6 +104,9 @@ def print_warp_label(type_, version, serial_number, date):
     with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'warp.prn'), 'rb') as f:
         data = f.read()
 
+    if data.find(b'^H13\r') < 0:
+        raise Exception('EZPL file is using wrong darkness setting')
+
     # patch QR code
     qr_code_command = QR_CODE_FORMAT.format(QR_CODE_LENGTH).encode('ascii')
 
