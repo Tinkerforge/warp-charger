df99c33e4317af3be8d12cb66910e7d331aab451
Matthias Bolte <matthias@tinkerforge.com>
2022-07-11 15:41:41 +0200

    accessories: Add stand option to label

--- label/print-accessories2-label-demo.sh
+++ label/print-accessories2-label-demo.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-./print-accessories2-label.py -s 432.1 1 "Lorem ipsum dolor sit amet consetetur" "sadipscing elitr, sed diam nonumy" "eirmod tempor invidunt ut labore et dolore"
+./print-accessories2-label.py -s 0 432.1 1 "Lorem ipsum dolor sit amet consetetur" "sadipscing elitr, sed diam nonumy" "eirmod tempor invidunt ut labore et dolore"
+echo ""
+./print-accessories2-label.py -s 1 0.0 0 "Lorem ipsum dolor sit amet consetetur" "sadipscing elitr, sed diam nonumy" "eirmod tempor invidunt ut labore et dolore"
--- label/print-accessories2-label.py
+++ label/print-accessories2-label.py
@@ -15,7 +15,8 @@ PRINTER_PORT = 9100
 QR_CODE_COMMAND = b'W649,209,5,2,M,8,6,58,0\r'
 QR_CODE_PADDING = b';;\r'
 
-SKU_PLACEHOLDER = b'WARP2-CS-11KW-50'
+STAND_PLACEHOLDER_A = b'Lades\xc3\xa4ule: Bitte'
+STAND_PLACEHOLDER_B = b'S:1;'
 
 SUPPLY_CABLE_PLACEHOLDER_A = b'Anschlusskabel: 123,4 m'
 SUPPLY_CABLE_PLACEHOLDER_B = b'E:123.4;'
@@ -29,7 +30,7 @@ COMMENT_3_PLACEHOLDER = b'Hinweise.'
 
 COPIES_FORMAT = '^C{0}\r'
 
-def print_accessories2_label(supply_cable, cee, comment_1, comment_2, comment_3, copies, stdout):
+def print_accessories2_label(stand, supply_cable, cee, comment_1, comment_2, comment_3, copies, stdout):
     # check copies
     if copies < 1 or copies > 5:
         raise Exception('Invalid copies: {0}'.format(copies))
@@ -58,6 +59,17 @@ def print_accessories2_label(supply_cable, cee, comment_1, comment_2, comment_3,
         raise Exception('QR code data too long')
 
     template = template.replace(QR_CODE_PADDING, b';' * offset + QR_CODE_PADDING)
+
+    # patch stand
+    if template.find(STAND_PLACEHOLDER_A) < 0:
+        raise Exception('Stand placeholder A missing in EZPL file')
+
+    template = template.replace(STAND_PLACEHOLDER_A, b'Lades\xc3\xa4ule: Ja' if stand else b'')
+
+    if template.find(STAND_PLACEHOLDER_B) < 0:
+        raise Exception('Stand placeholder B missing in EZPL file')
+
+    template = template.replace(STAND_PLACEHOLDER_B, 'S:{0};'.format(int(stand)).encode('ascii'))
 
     # patch supply cable
     if template.find(SUPPLY_CABLE_PLACEHOLDER_A) < 0:
@@ -118,6 +130,7 @@ def print_accessories2_label(supply_cable, cee, comment_1, comment_2, comment_3,
 def main():
     parser = argparse.ArgumentParser()
 
+    parser.add_argument('stand', type=int, choices=[1, 0])
     parser.add_argument('supply_cable', type=float)
     parser.add_argument('cee', type=int, choices=[1, 0])
     parser.add_argument('comment_1')
@@ -130,7 +143,7 @@ def main():
 
     assert args.copies > 0
 
-    print_accessories2_label(args.supply_cable, bool(args.cee), args.comment_1, args.comment_2, args.comment_3, args.copies, args.stdout)
+    print_accessories2_label(bool(args.stand), args.supply_cable, bool(args.cee), args.comment_1, args.comment_2, args.comment_3, args.copies, args.stdout)
 
 if __name__ == '__main__':
     main()
