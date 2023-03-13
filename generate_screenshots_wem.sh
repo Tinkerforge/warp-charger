fbea6a72acb184fafa8cbf618737e399469e6356
Erik Fleckstein <erik@tinkerforge.com>
2023-03-13 17:00:15 +0100

    manual_energy_manager: Update screenshots and wording.

--- generate_screenshots_wem.sh
+++ generate_screenshots_wem.sh
@@ -1,30 +1,30 @@
 #!/bin/sh
 
 #status
-python3 screenshot.py -o manual_energy_manager/img/resized/web_status.png -H $1 -p -c '#sidebar-energy_manager-group' '#sidebar-network-group' '#sidebar-interfaces-group' '#sidebar-system-group'
+python3 screenshot.py -o manual_energy_manager/img/resized/web_status.png -H $1 -p -c '#sidebar-energy_manager-group' '#sidebar-network-group' '#sidebar-interfaces-group' '#sidebar-system-group' --full 1280
 
-python3 screenshot.py -o manual_energy_manager/img/resized/web_em_energy_analysis.png -H $1 --crop -c '#sidebar-em-energy-analysis' -e '#em-energy-analysis'
+python3 screenshot.py -o manual_energy_manager/img/resized/web_em_energy_analysis.png -H $1 --crop -c '#sidebar-em-energy-analysis' -e '#em-energy-analysis' --full 1280
 
 #energy manager group
-python3 screenshot.py -o manual_energy_manager/img/resized/web_em_settings.png -H $1 --crop -c '#sidebar-energy_manager-group' '#sidebar-energy_manager' -e '#energy_manager'
-python3 screenshot.py -o manual_energy_manager/img/resized/web_em_meter_config.png -H $1 --crop -c '#sidebar-energy_manager-group' '#sidebar-em_meter_config' -e '#em_meter_config'
-python3 screenshot.py -o manual_energy_manager/img/resized/web_charge_manager.png -H $1 --crop -c '#sidebar-energy_manager-group' '#sidebar-charge_manager' -e '#charge_manager'
+python3 screenshot.py -o manual_energy_manager/img/resized/web_em_settings.png -H $1 --crop -c '#sidebar-energy_manager-group' '#sidebar-energy_manager' -e '#energy_manager' --full 1280
+python3 screenshot.py -o manual_energy_manager/img/resized/web_em_meter_config.png -H $1 --crop -c '#sidebar-energy_manager-group' '#sidebar-em_meter_config' -e '#em_meter_config' --full 1280
+python3 screenshot.py -o manual_energy_manager/img/resized/web_charge_manager.png -H $1 --crop -c '#sidebar-energy_manager-group' '#sidebar-charge_manager' -e '#charge_manager' --full 1280
 
 # network group
-python3 screenshot.py -o manual_energy_manager/img/resized/web_network.png -H $1 --crop -c '#sidebar-network-group' '#sidebar-network' -e '#network'
-python3 screenshot.py -o manual_energy_manager/img/resized/web_wifi_sta.png -H $1 --crop -c '#sidebar-network-group' '#sidebar-wifi-sta' -e '#wifi-sta'
-python3 screenshot.py -o manual_energy_manager/img/resized/web_wifi_ap.png -H $1 --crop -c '#sidebar-network-group' '#sidebar-wifi-ap' -e '#wifi-ap'
-python3 screenshot.py -o manual_energy_manager/img/resized/web_ethernet.png -H $1 --crop -c '#sidebar-network-group' '#sidebar-ethernet' -e '#ethernet'
-python3 screenshot.py -o manual_energy_manager/img/resized/web_wireguard.png -H $1 --crop -c '#sidebar-network-group' '#sidebar-wireguard' -e '#wireguard'
+python3 screenshot.py -o manual_energy_manager/img/resized/web_network.png -H $1 --crop -c '#sidebar-network-group' '#sidebar-network' -e '#network' --full 1280
+python3 screenshot.py -o manual_energy_manager/img/resized/web_wifi_sta.png -H $1 --crop -c '#sidebar-network-group' '#sidebar-wifi-sta' -e '#wifi-sta' --full 1280
+python3 screenshot.py -o manual_energy_manager/img/resized/web_wifi_ap.png -H $1 --crop -c '#sidebar-network-group' '#sidebar-wifi-ap' -e '#wifi-ap' --full 1280
+python3 screenshot.py -o manual_energy_manager/img/resized/web_ethernet.png -H $1 --crop -c '#sidebar-network-group' '#sidebar-ethernet' -e '#ethernet' --full 1280
+python3 screenshot.py -o manual_energy_manager/img/resized/web_wireguard.png -H $1 --crop -c '#sidebar-network-group' '#sidebar-wireguard' -e '#wireguard' --full 1280
 
 # interfaces group
-python3 screenshot.py -o manual_energy_manager/img/resized/web_mqtt.png -H $1 --crop -c '#sidebar-interfaces-group' '#sidebar-mqtt' -e '#mqtt'
+python3 screenshot.py -o manual_energy_manager/img/resized/web_mqtt.png -H $1 --crop -c '#sidebar-interfaces-group' '#sidebar-mqtt' -e '#mqtt' --full 1280
 
 # system group
-python3 screenshot.py -o manual_energy_manager/img/resized/web_ntp.png -H $1 --crop -c '#sidebar-system-group' '#sidebar-ntp' -e '#ntp'
-python3 screenshot.py -o manual_energy_manager/img/resized/web_ntp.png -H $1 --crop -c '#sidebar-system-group' '#sidebar-em_sdcard' -e '#em_sdcard'
-python3 screenshot.py -o manual_energy_manager/img/resized/web_ntp.png -H $1 --crop -c '#sidebar-system-group' '#sidebar-authentication' -e '#authentication'
-python3 screenshot.py -o manual_energy_manager/img/resized/web_event_log.png -H $1 --crop -c '#sidebar-system-group' '#sidebar-event_log' -e '#event_log'
-python3 screenshot.py -o manual_energy_manager/img/resized/web_firmware_update.png -H $1 --crop -c '#sidebar-system-group' '#sidebar-flash' -e '#flash'
+python3 screenshot.py -o manual_energy_manager/img/resized/web_ntp.png -H $1 --crop -c '#sidebar-system-group' '#sidebar-ntp' -e '#ntp' --full 1280
+python3 screenshot.py -o manual_energy_manager/img/resized/web_em_sdcard.png -H $1 --crop -c '#sidebar-system-group' '#sidebar-em_sdcard' -e '#em_sdcard' --full 1280
+python3 screenshot.py -o manual_energy_manager/img/resized/web_authentication.png -H $1 --crop -c '#sidebar-system-group' '#sidebar-authentication' -e '#authentication' --full 1280
+python3 screenshot.py -o manual_energy_manager/img/resized/web_event_log.png -H $1 --crop -c '#sidebar-system-group' '#sidebar-event_log' -e '#event_log' --full 1280
+python3 screenshot.py -o manual_energy_manager/img/resized/web_firmware_update.png -H $1 --crop -c '#sidebar-system-group' '#sidebar-flash' -e '#flash' --full 1280
 
 echo "Manually crop web_status.png!"
