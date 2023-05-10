04ad5b5159df7225023918cd8856fd9766fc3b72
Matthias Bolte <matthias@tinkerforge.com>
2023-05-10 11:33:05 +0200

    label: More lines on internal label and less official look

--- label/print-internal2-label-demo.sh
+++ label/print-internal2-label-demo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./print-internal2-label.py -s "Lorem ipsum dolor sit amet consetetur" "sadipscing elitr, sed diam nonumy" "eirmod tempor invidunt ut labore et dolore" "4" "5" "6"
+./print-internal2-label.py -s "Lorem ipsum dolor sit amet consetetur" "sadipscing elitr, sed diam nonumy" "eirmod tempor invidunt ut labore et dolore" "4" "5" "6" "7" "8"
--- label/print-internal2-label.py
+++ label/print-internal2-label.py
@@ -11,6 +11,8 @@ INTERNAL_3_PLACEHOLDER = b'Hinweise und Gedanke.'
 INTERNAL_4_PLACEHOLDER = b'Was man sich auch'
 INTERNAL_5_PLACEHOLDER = b'immer intern mitteilen will,'
 INTERNAL_6_PLACEHOLDER = b'hier ist Platz daf\xC3\xBCr.'
+INTERNAL_7_PLACEHOLDER = b'Und noch zwei'
+INTERNAL_8_PLACEHOLDER = b'weitere Zeilen.'
 
 COPIES_FORMAT = '^C{0}\r'
 
@@ -60,7 +62,7 @@ def get_tf_printer_host(task):
     sys.exit(1)
 
 
-def print_internal2_label(internal_1, internal_2, internal_3, internal_4, internal_5, internal_6, copies, stdout):
+def print_internal2_label(internal_1, internal_2, internal_3, internal_4, internal_5, internal_6, internal_7, internal_8, copies, stdout):
     # check copies
     if copies < 1 or copies > 5:
         raise Exception('Invalid copies: {0}'.format(copies))
@@ -108,6 +110,18 @@ def print_internal2_label(internal_1, internal_2, internal_3, internal_4, intern
 
     template = template.replace(INTERNAL_6_PLACEHOLDER, internal_6.encode('latin1', errors='replace'))
 
+    # patch internal 7
+    if template.find(INTERNAL_7_PLACEHOLDER) < 0:
+        raise Exception('Internal 7 placeholder missing in EZPL file')
+
+    template = template.replace(INTERNAL_7_PLACEHOLDER, internal_7.encode('latin1', errors='replace'))
+
+    # patch internal 8
+    if template.find(INTERNAL_8_PLACEHOLDER) < 0:
+        raise Exception('Internal 8 placeholder missing in EZPL file')
+
+    template = template.replace(INTERNAL_8_PLACEHOLDER, internal_8.encode('latin1', errors='replace'))
+
     # patch copies
     copies_command = COPIES_FORMAT.format(1).encode('ascii')
 
@@ -134,6 +148,8 @@ def main():
     parser.add_argument('internal_4')
     parser.add_argument('internal_5')
     parser.add_argument('internal_6')
+    parser.add_argument('internal_7')
+    parser.add_argument('internal_8')
     parser.add_argument('-c', '--copies', type=int, default=1)
     parser.add_argument('-s', '--stdout', action='store_true')
 
@@ -141,7 +157,7 @@ def main():
 
     assert args.copies > 0
 
-    print_internal2_label(args.internal_1, args.internal_2, args.internal_3, args.internal_4, args.internal_5, args.internal_6, args.copies, args.stdout)
+    print_internal2_label(args.internal_1, args.internal_2, args.internal_3, args.internal_4, args.internal_5, args.internal_6, args.internal_7, args.internal_8, args.copies, args.stdout)
 
 
 if __name__ == '__main__':
