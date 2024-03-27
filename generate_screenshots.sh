5102c5c45b01aa3732b4fbce0937440567ff5e56
Erik Fleckstein <erik@tinkerforge.com>
2024-03-27 13:42:02 +0100

    Update screenshot script for WARP3.

--- generate_screenshots.sh
+++ generate_screenshots.sh
@@ -2,39 +2,45 @@
 
 # status
 
-python3 screenshot.py -o manual/img_warp2/resized/web_status.png -H $1 -c '#sidebar-evse-group' '#sidebar-network-group' '#sidebar-interfaces-group' '#sidebar-users-group' '#sidebar-system-group' --full
+python3 screenshot.py -o manual/img_warp3/resized/web_status.png -H $1#status -c '#nav-list > li:nth-child(2) > div:nth-child(1)' '#nav-list > li:nth-child(3) > div:nth-child(1)' '#nav-list > li:nth-child(4) > div:nth-child(1)' '#nav-list > li:nth-child(5) > div:nth-child(1)' '#nav-list > li:nth-child(6) > div:nth-child(1)' '#nav-list > li:nth-child(7) > div:nth-child(1)' --full
 
 # evse group
-python3 screenshot.py -o manual/img_warp2/resized/web_evse2.png -H $1 --crop -c '#sidebar-evse-group' '#sidebar-evse' -e '#evse' -l '#evse > div:nth-child(1) > div:nth-child(1) > div:nth-child(31)' --full -w 1016
-python3 screenshot.py -o manual/img_warp2/resized/web_evse2_settings.png -H $1 --crop -c '#sidebar-evse-group' '#sidebar-evse-settings' -e '#evse-settings' --full
+python3 screenshot.py -o manual/img_warp3/resized/web_evse_settings.png -H $1#evse_settings --crop -e '.tab-content' --full
+python3 screenshot.py -o manual/img_warp3/resized/web_evse.png -H $1#evse --crop -e '.tab-content' --full -w 1016
+python3 screenshot.py -o manual/img_warp3/resized/web_charge_tracker.png -H $1#charge_tracker --crop -e '.tab-content' --full
+python3 screenshot.py -o manual/img_warp3/resized/web_automation.png -H $1#automation --crop -e '.tab-content' --full
 
-python3 screenshot.py -o manual/img_warp2/resized/web_meter.png -H $1 --crop -c '#sidebar-evse-group' '#sidebar-meters' -e '#meters' --full
-python3 screenshot.py -o manual/img_warp2/resized/web_charge_tracker.png -H $1 --crop -c '#sidebar-evse-group' '#sidebar-charge_tracker' -e '#charge_tracker' --full
-python3 screenshot.py -o manual/img_warp2/resized/web_charge_manager.png -H $1 --crop -c '#sidebar-evse-group' '#sidebar-charge_manager' -e '#charge_manager' --full
+#energy management group
+python3 screenshot.py -o manual/img_warp3/resized/web_meters.png -H $1#meters --crop -e '.tab-content' --full
+python3 screenshot.py -o manual/img_warp3/resized/web_charge_manager_chargers.png -H $1#charge_manager_chargers --crop -e '.tab-content' --full
+python3 screenshot.py -o manual/img_warp3/resized/web_pv_excess_settings.png -H $1#pv_excess_settings --crop -e '.tab-content' --full
+python3 screenshot.py -o manual/img_warp3/resized/web_charge_manager_settings.png -H $1#charge_manager_settings --crop -e '.tab-content' --full
 
 # network group
-python3 screenshot.py -o manual/img_warp2/resized/web_network.png -H $1 --crop -c '#sidebar-network-group' '#sidebar-network' -e '#network' --full
-python3 screenshot.py -o manual/img_warp2/resized/web_wifi_ap.png -H $1 --crop -c '#sidebar-network-group' '#sidebar-wifi-ap' -e '#wifi-ap' --full
-python3 screenshot.py -o manual/img_warp2/resized/web_wifi_sta.png -H $1 --crop -c '#sidebar-network-group' '#sidebar-wifi-sta' -e '#wifi-sta' --full
-python3 screenshot.py -o manual/img_warp2/resized/web_ethernet.png -H $1 --crop -c '#sidebar-network-group' '#sidebar-ethernet' -e '#ethernet' --full
-python3 screenshot.py -o manual/img_warp2/resized/web_wireguard.png -H $1 --crop -c '#sidebar-network-group' '#sidebar-wireguard' -e '#wireguard' --full
+python3 screenshot.py -o manual/img_warp3/resized/web_network.png -H $1#network --crop -e '.tab-content' --full
+python3 screenshot.py -o manual/img_warp3/resized/web_wifi_sta.png -H $1#wifi_sta --crop -e '.tab-content' --full
+python3 screenshot.py -o manual/img_warp3/resized/web_wifi_ap.png -H $1#wifi_ap --crop -e '.tab-content' --full
+python3 screenshot.py -o manual/img_warp3/resized/web_ethernet.png -H $1#ethernet --crop -e '.tab-content' --full
+python3 screenshot.py -o manual/img_warp3/resized/web_wireguard.png -H $1#wireguard --crop -e '.tab-content' --full
 
 # interfaces group
 
-python3 screenshot.py -o manual/img_warp2/resized/web_mqtt.png -H $1 --crop -c '#sidebar-interfaces-group' '#sidebar-mqtt' -e '#mqtt' --full
-python3 screenshot.py -o manual/img_warp2/resized/web_modbus_tcp.png -H $1 --crop -c '#sidebar-interfaces-group' '#sidebar-modbus_tcp' -e '#modbus_tcp' --full
-python3 screenshot.py -o manual/img_warp2/resized/web_ocpp.png -H $1 --crop -c '#sidebar-interfaces-group' '#sidebar-ocpp' -e '#ocpp' --full
+python3 screenshot.py -o manual/img_warp3/resized/web_mqtt.png -H $1#mqtt --crop -e '.tab-content' --full
+python3 screenshot.py -o manual/img_warp3/resized/web_modbus_tcp.png -H $1#modbus_tcp --crop -e '.tab-content' --full
+python3 screenshot.py -o manual/img_warp3/resized/web_ocpp.png -H $1#ocpp --crop -e '.tab-content' --full
 
 # users group
 
-python3 screenshot.py -o manual/img_warp2/resized/web_nfc.png -H $1 --crop -c '#sidebar-users-group' '#sidebar-nfc' -e '#nfc' --full
-python3 screenshot.py -o manual/img_warp2/resized/web_users.png -H $1 --crop -c '#sidebar-users-group' '#sidebar-users' -e '#users' --full
+python3 screenshot.py -o manual/img_warp3/resized/web_nfc.png -H $1#nfc --crop -e '.tab-content' --full
+python3 screenshot.py -o manual/img_warp3/resized/web_users.png -H $1#users --crop -e '.tab-content' --full
 
 # system group
-python3 screenshot.py -o manual/img_warp2/resized/web_ntp.png -H $1 --crop -c '#sidebar-system-group' '#sidebar-ntp' -e '#ntp' --full
-python3 screenshot.py -o manual/img_warp2/resized/web_event_log.png -H $1 --crop -c '#sidebar-system-group' '#sidebar-event_log' -e '#event_log' --full
-python3 screenshot.py -o manual/img_warp2/resized/web_firmware_update.png -H $1 --crop -c '#sidebar-system-group' '#sidebar-flash' -e '#flash' --full
+python3 screenshot.py -o manual/img_warp3/resized/web_certs.png -H $1#certs --crop -e '.tab-content' --full
+python3 screenshot.py -o manual/img_warp3/resized/web_ntp.png -H $1#ntp --crop -e '.tab-content' --full
+python3 screenshot.py -o manual/img_warp3/resized/web_rtc.png -H $1#rtc --crop -e '.tab-content' --full
+python3 screenshot.py -o manual/img_warp3/resized/web_event_log.png -H $1#event_log --crop -e '.tab-content' --full
+python3 screenshot.py -o manual/img_warp3/resized/web_firmware_update.png -H $1#firmware_update --crop -e '.tab-content' --full
 
-cp manual/img_warp2/resized/web_status_full.png warp-charger.com/img_warp2/impressions/web3.png
-cp manual/img_warp2/resized/web_meter_full.png warp-charger.com/img_warp2/impressions/web1.png
-cp manual/img_warp2/resized/web_wifi_sta_full.png warp-charger.com/img_warp2/impressions/web2.png
+cp manual/img_warp3/resized/web_status_full.png warp-charger.com/img_warp3/impressions/web3.png
+cp manual/img_warp3/resized/web_meter_full.png warp-charger.com/img_warp3/impressions/web1.png
+cp manual/img_warp3/resized/web_wifi_sta_full.png warp-charger.com/img_warp3/impressions/web2.png
