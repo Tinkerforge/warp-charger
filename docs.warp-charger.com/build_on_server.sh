123fa56b8754c98e3686e2cf8f375fb7be17b6c3
Olaf Lüke <olaf@tinkerforge.com>
2024-04-15 13:14:01 +0200

    Add simple auto-deploy script

---
+++ docs.warp-charger.com/build_on_server.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Simple auto-deploy from github
+# This is called on docs.warp-charger.com with cron
+cd /var/www/git/warp-charger/docs.warp-charger.com/
+/usr/bin/git pull origin master
+/usr/local/bin/npm run build
+cp .htaccess build/
+cp .htpasswd build/
