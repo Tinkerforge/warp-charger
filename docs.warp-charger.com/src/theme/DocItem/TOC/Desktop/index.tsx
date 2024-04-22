5fe58c2dda2867c8222cce3d067ba617f512e34b
Erik Fleckstein <erik@tinkerforge.com>
2024-04-22 15:59:22 +0200

    docs: Fix hardware selection.

--- docs.warp-charger.com/src/theme/DocItem/TOC/Desktop/index.tsx
+++ docs.warp-charger.com/src/theme/DocItem/TOC/Desktop/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Desktop from '@theme-original/DocItem/TOC/Desktop';
 import type DesktopType from '@theme/DocItem/TOC/Desktop';
 import type { WrapperProps } from '@docusaurus/types';
@@ -17,6 +17,11 @@ import styles from '@docusaurus/theme-classic/src/theme/TOC/styles.module.css';
 
 type Props = WrapperProps<typeof DesktopType>;
 
+let interval: NodeJS.Timeout = null;
+let scrollHandled = "foobar";
+
+const hardwareTypes = ['warp1', 'warp2', 'warp3', 'wem', 'any'];
+
 export default function DocItemTOCDesktop(props: Props): JSX.Element {
     const { toc, frontMatter, metadata } = useDoc();
 
@@ -26,6 +31,57 @@ export default function DocItemTOCDesktop(props: Props): JSX.Element {
 
     // We are in the API documentation.
     // Show the api/hardwareType tabs and filter the TOC.
+
+    // The user selected hardware type is stored in the localStorage and part of the query string.
+    // If the page is loaded with a fragment that does not fit to
+    // the hardware type (fragments are suffixed with the hardware
+    // type of the function), rewrite the fragment to the selected
+    // type so that the browser scrolls to the correct function.
+    // Also allow setting the fragment _once_ even if it is already
+    // correct to force the browser to scroll to the function if
+    // the hardware type was not rendered in the first run.
+    if (interval == null) {
+        // Use effect to make sure the interval always runs after the first render
+        useEffect(() => {
+            interval = setInterval(() => {
+                let storedHardwareType = window?.localStorage?.getItem("docusaurus.tab.hardwareType");
+                let hardwareType = new URLSearchParams(window.location.search)?.get("hardwareType") ?? storedHardwareType;
+
+                if (hardwareType == null) {
+                    scrollHandled = "foobar";
+                    return;
+                }
+
+                let hash = window.location.hash;
+
+                if (hash.length == 0) {
+                    scrollHandled = "foobar";
+                    return;
+                }
+
+                let found = false;
+                for (let x of hardwareTypes) {
+                    if (hash.endsWith(x)) {
+                        found = true;
+                        let r = new RegExp(x + '$');
+                        hash = hash.replace(r, hardwareType);
+                        break;
+                    }
+                }
+
+                if (!found) {
+                    hash = hash + "_" + hardwareType;
+                }
+
+                if (window.location.hash == hash && scrollHandled == hardwareType + hash)
+                    return;
+
+                scrollHandled = hardwareType + hash;
+                history.replaceState(null, "", window.location.pathname + hash)
+                window.location.hash = hash;
+            }, 100);
+        }, []);
+    }
 
     const filterToC = (toc: readonly TOCItem[], hardwareType: string) => {
         let level_filter = -1;
@@ -38,14 +94,14 @@ export default function DocItemTOCDesktop(props: Props): JSX.Element {
             if (level_filter != -1 && x.level <= level_filter)
                 level_filter = -1;
 
-            if (hardwareType != "all"
+            if (hardwareType != "any"
                 && "hardwareType" in x
                 && (x.hardwareType as string[]).indexOf(hardwareType) == -1) {
                 level_filter = x.level;
                 continue;
             }
 
-            result.push(x);
+            result.push({...x, id: x.id + "_" + hardwareType});
         }
 
         return result;
@@ -64,29 +120,11 @@ export default function DocItemTOCDesktop(props: Props): JSX.Element {
         </MultilineTabs>
         <hr />
         <MultilineTabs groupId="hardwareType" queryString className='unique-tabs'>
-            <TabItem value="warp1" label="WARP1 Charger">
-                <hr />
-                <MultilineTabs groupId="chargerVariant" queryString className='unique-tabs'>
-                    <TabItem value="smart" label="Smart"> </TabItem>
-                    <TabItem value="pro" label="Pro"> </TabItem>
-                </MultilineTabs>
-            </TabItem>
-            <TabItem value="warp2" label="WARP2 Charger">
-                <hr />
-                <MultilineTabs groupId="chargerVariant" queryString className='unique-tabs'>
-                    <TabItem value="smart" label="Smart"> </TabItem>
-                    <TabItem value="pro" label="Pro"> </TabItem>
-                </MultilineTabs>
-            </TabItem>
-            <TabItem value="warp3" label="WARP3 Charger">
-                <hr />
-                <MultilineTabs groupId="chargerVariant" queryString className='unique-tabs'>
-                    <TabItem value="smart" label="Smart"> </TabItem>
-                    <TabItem value="pro" label="Pro"> </TabItem>
-                </MultilineTabs>
-            </TabItem>
+            <TabItem value="warp1" label="WARP1 Charger"> </TabItem>
+            <TabItem value="warp2" label="WARP2 Charger"> </TabItem>
+            <TabItem value="warp3" label="WARP3 Charger"> </TabItem>
             <TabItem value="wem" label="WARP Energy Manager"> </TabItem>
-            <TabItem value="all" label="Alle Typen"> </TabItem>
+            <TabItem value="any" label="Alle Typen"> </TabItem>
         </MultilineTabs>
 
         <hr />
@@ -104,8 +142,8 @@ export default function DocItemTOCDesktop(props: Props): JSX.Element {
             <TabItem value="wem" label="WARP Energy Manager">
                 <TOC toc={filterToC(toc, "wem")} {...tocprops} />
             </TabItem>
-            <TabItem value="all" label="Alle Typen">
-                <TOC toc={toc} {...tocprops} />
+            <TabItem value="any" label="Alle Typen">
+                <TOC toc={filterToC(toc, "any")} {...tocprops} />
             </TabItem>
         </Tabs>
     </div>;
