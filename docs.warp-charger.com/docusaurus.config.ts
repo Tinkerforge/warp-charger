3ff46b9d76205b77322e113ded0c2d5dde19f8ea
Olaf Lüke <olaf@tinkerforge.com>
2024-05-06 22:58:20 +0200

    Add initial algolia search test
    Note: Ot is OK to commit the ID und and key.
    This is all public information.

--- docs.warp-charger.com/docusaurus.config.ts
+++ docs.warp-charger.com/docusaurus.config.ts
@@ -48,6 +48,38 @@ const config: Config = {
   ],
 
   themeConfig: {
+    algolia: {
+      // The application ID provided by Algolia
+      appId: 'WDTZV3KHAX',
+
+      // Public API key: it is safe to commit it
+      apiKey: 'ebf6f72368963d7ad6070e373b771762',
+
+      indexName: 'warp-charger',
+
+      // Optional: see doc section below
+//      contextualSearch: true,
+
+      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+//      externalUrlRegex: '',
+
+      // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+//      replaceSearchResultPathname: {
+//	     from: '/docs/', // or as RegExp: /\/docs\//
+//        to: '/',
+//      },
+
+      // Optional: Algolia search parameters
+//      searchParameters: {},
+
+      // Optional: path for search page that enabled by default (`false` to disable it)
+//      searchPagePath: 'search',
+
+      // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+//      insights: false,
+
+      //... other Algolia params
+    },
     // Replace with your project's social card
     // image: '',
     navbar: {
