cd3169bab104bb2bf754adcfa5bb42319a1b8e4a
Olaf Lüke <olaf@tinkerforge.com>
2024-05-07 00:05:26 +0200

    First build, then move build files to release folder

--- docs.warp-charger.com/build_on_server.sh
+++ docs.warp-charger.com/build_on_server.sh
@@ -3,6 +3,6 @@
 # Simple auto-deploy from github
 # This is called on docs.warp-charger.com with cron
 cd /var/www/git/warp-charger/docs.warp-charger.com/
-cp -r build/* build_release/
 /usr/bin/git pull origin master
 /usr/local/bin/npm run build
+cp -r build/* build_release/
