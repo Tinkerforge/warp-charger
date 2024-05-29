bfedbb85408e9929e489b42c5fff0be29e4d3e49
Matthias Bolte <matthias@tinkerforge.com>
2024-05-29 17:48:18 +0200

    Fix typos

--- api_doc_generator/info.py
+++ api_doc_generator/info.py
@@ -18,7 +18,7 @@ info = Module("info", "Allgemeine Informationen", "", "", Version.ANY, [
                 Const("warp3", "Ein WARP3 Charger"),
                 Const("wem", "Ein WARP Energy Manager"),
             ]),
-            "display_type": Elem.STRING("Benutzerlesbarer Typ des Geräts. Gibt neben der Hardware-Version die Variante (Smart, Pro), die maximale Ladeleistung und eventuelle Upgrades an. z.B. \"WARP2 Charger Pro 11kW +NFC\""),
+            "display_type": Elem.STRING("Benutzerlesbarer Typ des Geräts. Gibt neben der Hardware-Version die Variante (Smart, Pro), die maximale Ladeleistung und eventuelle Upgrades an, z.B. \"WARP2 Charger Pro 11kW +NFC\""),
             "uid": Elem.STRING("UID des Geräts bzw. des verbauten ESP (Ethernet) Bricks.")
         })
     ),
@@ -29,8 +29,8 @@ info = Module("info", "Allgemeine Informationen", "", "", Version.ANY, [
 
     Func("last_boots", FuncType.STATE, Elem.ARRAY("Debug-Informationen über die letzten Firmware-Ausführungen",  members=[
                 * 10 * [Elem.OBJECT("Debug-Informationen über eine Firmware-Ausführung", members = {
-            "reset_reason": Elem.INT("Grund des Neustarts. Siehe [Espressif-Dokumentation](https://docs.espressif.com/projects/esp-idf/en/v4.4.3/esp32/api-reference/system/system.html?highlight=esp_reset_reason#_CPPv418esp_reset_reason_t)"),
-            "boot_count": Elem.INT("Zähler der angibt, der wievielte Neustart seit dem das Gerät vom Strom getrennt wurde, durchgeführt wird. Ein Eintrag in last_boots wird erst fünf Minuten nach dem Start des Geräts geschrieben um den Flash zu schonen. Falls innerhalb der ersten fünf Minuten neugestartet wird, kann ein Eintrag verloren gehen, was anhand dieses Zählers bemerkt werden kann"),
+            "reset_reason": Elem.INT("Grund des Neustarts. Siehe [Espressif-Dokumentation](https://docs.espressif.com/projects/esp-idf/en/v4.4.3/esp32/api-reference/system/system.html?highlight=esp_reset_reason#_CPPv418esp_reset_reason_t)."),
+            "boot_count": Elem.INT("Zähler der angibt, der wievielte Neustart seit dem das Gerät vom Strom getrennt wurde, durchgeführt wird. Ein Eintrag in last_boots wird erst fünf Minuten nach dem Start des Geräts geschrieben um den Flash zu schonen. Falls innerhalb der ersten fünf Minuten neugestartet wird, kann ein Eintrag verloren gehen, was anhand dieses Zählers bemerkt werden kann."),
             "uptime": Elem.INT("Laufzeit der Firmware-Ausführung (32-Bit).", unit=Units.ms),
             "uptime_overflows": Elem.INT("Anzahl der Überläufe von uptime."),
             "timestamp_min": Elem.INT("Unix-Timestamp in Minuten der Firmware-Ausführung.")
