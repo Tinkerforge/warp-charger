95749ed172f5f8920b3ca3e90dd6ad48d7d51109
Erik Fleckstein <erik@tinkerforge.com>
2024-06-04 15:21:10 +0200

    api_docs: Fix require_meter/config constants.

--- api_doc_generator/require_meter.py
+++ api_doc_generator/require_meter.py
@@ -3,9 +3,9 @@ from api_doc_common import *
 require_meter = Module("require_meter", "Zählerüberwachung", "", "", Version.CHARGER, [
     Func("config", FuncType.CONFIGURATION, Elem.OBJECT("Die Konfiguration der Zählerüberwachung.", members={
         "config": Elem.INT("Konfiguration der Zählerüberwachung.", constants=[
-            Const(0, "Es wurde noch nie ein Stromzähler gesehen. Zählerüberwachung deaktiviert. Sobald das erste Mal ein Stromzähler erkannt wird, wird dieser Wert automatisch auf 1 geändert."),
-            Const(1, "Es wurde bereits einmal ein Stromzähler gesehen. Zählerüberwachung aktiviert."),
-            Const(2, "Es wurde bereits einmal ein Stromzähler gesehen. Zählerüberwachung deaktiviert."),
+            Const(0, "Es wurde noch nie ein Stromzähler gesehen. Zählerüberwachung deaktiviert. Sobald das erste Mal ein Stromzähler erkannt wird, wird dieser Wert automatisch auf 2 geändert."),
+            Const(1, "Es wurde bereits einmal ein Stromzähler gesehen. Zählerüberwachung deaktiviert."),
+            Const(2, "Es wurde bereits einmal ein Stromzähler gesehen. Zählerüberwachung aktiviert."),
         ])
     }))
 ])
