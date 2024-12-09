2bd99f3c7334ae27801c94855534d5f98c71e316
Erik Fleckstein <erik@tinkerforge.com>
2024-12-09 13:16:09 +0100

    api_doc_generator: Add new modules to module list.

--- api_doc_generator/mods.py
+++ api_doc_generator/mods.py
@@ -24,5 +24,9 @@ from automation import automation
 from meters import meters
 from meters_legacy_api import meters_legacy_api
 from power_manager import power_manager
+from day_ahead_prices import day_ahead_prices
+from front_panel import front_panel
+from heating import heating
+from solar_forecast import solar_forecast
 
-mods = [evse, meters, require_meter, charge_manager, users, charge_tracker, nfc, network, wifi, ethernet, ntp, mqtt, modbus_tcp, ocpp, wireguard, rtc, certs, info, authentication, misc, energy_manager, power_manager, charge_limits, automation, meters_legacy_api, meter]
+mods = [evse, meters, require_meter, charge_manager, users, charge_tracker, nfc, network, wifi, ethernet, ntp, mqtt, modbus_tcp, ocpp, wireguard, rtc, certs, info, authentication, misc, energy_manager, power_manager, charge_limits, automation, meters_legacy_api, meter, day_ahead_prices, front_panel, heating, solar_forecast]
