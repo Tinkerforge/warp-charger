56794a4de6f3e6c2e9b6b6c9fc63cc72e85d03b9
Olaf Lüke <olaf@tinkerforge.com>
2024-12-11 16:07:26 +0100

    Add docs for DAP, FP, SF and Heating

--- api_doc_generator/day_ahead_prices.py
+++ api_doc_generator/day_ahead_prices.py
@@ -3,33 +3,36 @@ from api_doc_common import *
 day_ahead_prices = Module("day_ahead_prices", "Dynamische Strompreise", "", "Über das `day_ahead_prices`-Modul können dynamische Strompreise konfiguriert und ausgelesen werden.", Version.ANY, [
     Func("config", FuncType.CONFIGURATION, Elem.OBJECT("Die Konfiguration der dynamischen Strompreise.", members={
         "enable": Elem.BOOL("Gibt an, ob dynamische Strompreise verwendet werden sollen. Wenn aktiviert, werden die Preise einmal pro Tag für die nächsten 24 Stunden von einem externen Server abgerufen. Um dynamische Strompreise nutzen zu können wird ein Internetzugang benötigt."),
-        "api_url":  Elem.STRING(""),
-        "region":  Elem.INT("", constants=[
+        "api_url":  Elem.STRING("Endpoint-URL des Servers für dynamische Strompreise."),
+        "region":  Elem.INT("Gebiet der dynmischen Strompreise", constants=[
             Const(0, "DE"),
             Const(1, "AT"),
             Const(2, "LU"),
         ]),
-        "resolution":  Elem.INT("", constants=[
+        "resolution":  Elem.INT("Auflösung der dynamischen Strompreise.", constants=[
             Const(0, "15 Minuten"),
             Const(1, "60 Minuten"),
         ]),
-        "cert_id":  Elem.INT(""),
-        "vat":  Elem.INT(""),
-        "grid_costs_and_taxes":  Elem.INT(""),
-        "supplier_markup":  Elem.INT(""),
-        "supplier_base_fee":  Elem.INT(""),
+        "cert_id":  Elem.INT("ID des CA-Zertifikats, dass zur Prüfung des Zertifikats des RADIUS-Servers genutzt wird. Siehe {{{ref:certs/state}}}."),
+        "vat":  Elem.INT("Mehrwersteuersatz der auf den Börsenstrompreis anfällt.", unit=Units.hundredth_percent),
+        "grid_costs_and_taxes":  Elem.INT("Netzentgelte und Steuern (brutto falls MwSt-Satz angegeben ist).", unit=Units.thousands_cent_per_kWh),
+        "supplier_markup":  Elem.INT("Stromanbieter Preisaufschlag (brutto falls MwSt-Satz angegeben ist).", unit=Units.thousands_cent_per_kWh),
+        "supplier_base_fee":  Elem.INT("Stromanbieter Grundgebühr", unit=Units.ct_per_month),
     })),
 
     Func("state", FuncType.STATE, Elem.OBJECT("Der Zustand der dynamischen Strompreise.", members={
-        "last_sync": Elem.INT(""),
-        "last_check": Elem.INT(""),
-        "next_check": Elem.INT(""),
-        "current_price": Elem.INT(""),
+        "last_sync": Elem.INT("Ein Unix-Timestamp in <strong>Minuten</strong>, der angibt wann die Börsenstrompreise das letzte Mal aktualisiert wurden."),
+        "last_check": Elem.INT("Ein Unix-Timestamp in <strong>Minuten</strong>, der angibt wann die Börsenstrompreise das letzte Mal abgefragt wurden."),
+        "next_check": Elem.INT("Ein Unix-Timestamp in <strong>Minuten</strong>, der angibt wann die Börsenstrompreise das nächste Mal abgefragt werden."),
+        "current_price": Elem.INT("Der aktuelle Börsenstrompreis.", unit=Units.thousands_cent_per_kWh),
     })),
 
     Func("prices", FuncType.STATE, Elem.OBJECT("Die dynamischen Strompreise.", members={
-        "first_date":  Elem.INT(""),
-        "resolution":  Elem.INT(""),
-        "prices": Elem.ARRAY("", members=25*4*2 * [Elem.INT("")])
+        "first_date":  Elem.INT("Ein Unix-Timestamp in <strong>Minuten</strong>, der angibt zu welchem Zeitpunkt der erste Preise im `prices`-Array gilt."),
+        "resolution":  Elem.INT("Auflösung der dynamischen Strompreise im `prices`-Array.", constants=[
+            Const(0, "15 Minuten"),
+            Const(1, "60 Minuten"),
+        ]),
+        "prices": Elem.ARRAY("Array von dynamischen Strompreisen für maximal zwei Tage. Das erste Element des Arrays ist für den Zeitpunkt `first_date` und die Auflösung legt fest wie weit die Elemente zueinander zeitlich versetzt sind.", members=25*4*2 * [Elem.INT("")], unit=Units.thousands_cent_per_kWh),
     })),
 ])
--- api_doc_generator/heating.py
+++ api_doc_generator/heating.py
@@ -2,10 +2,16 @@ from api_doc_common import *
 
 heating = Module("heating", "Heizung", "", "Über das `heating`-Modul können Einstellungen für die Heizungssteuerung vorgenommen werden.", Version.ANY, [
     Func("config", FuncType.CONFIGURATION, Elem.OBJECT("Die Konfiguration der Heizungssteuerung.", members={
-        "sgr_blocking_type": Elem.INT(""),
-        "sgr_extended_type": Elem.INT(""),
-        "min_hold_time": Elem.INT(""),
-        "meter_slot_grid_power": Elem.INT(""),
+        "sgr_blocking_type": Elem.INT("SG-Ready Ausgang 1 Konfiguration (wird für den blockierenden Betrieb verwendet).", constants=[
+            Const(0, "Aktiv wenn geschlossen"),
+            Const(1, "Aktiv wenn geöffnet"),
+        ]),
+        "sgr_extended_type": Elem.INT("SG-Ready Ausgang 2 Konfiguration (wird für den erweiterten Betrieb verwendet).", constants=[
+            Const(0, "Aktiv wenn geschlossen"),
+            Const(1, "Aktiv wenn geöffnet"),
+        ]),
+        "min_hold_time": Elem.INT("Mindesthaltezeitfür SG-Ready-Ausgang 1 und SG-Ready-Ausgang 2."),
+        "meter_slot_grid_power": Elem.INT("Gibt an, welcher Stromzähler für die Regelung als Hausanschlusszähler betrachtet wird."),
         "control_period":  Elem.INT("", constants=[
             Const(0, "24 Stunden"),
             Const(1, "12 Stunden"),
@@ -13,24 +19,32 @@ heating = Module("heating", "Heizung", "", "Über das `heating`-Modul können Ei
             Const(3, "6 Stunden"),
             Const(4, "4 Stunden"),
         ]),
-        "extended_logging": Elem.BOOL(""),
-        "yield_forecast": Elem.BOOL(""),
-        "yield_forecast_threshold": Elem.INT(""),
-        "extended": Elem.BOOL(""),
-        "extended_hours": Elem.INT(""),
-        "blocking": Elem.BOOL(""),
-        "blocking_hours": Elem.INT(""),
-        "pv_excess_control": Elem.BOOL(""),
-        "pv_excess_control_threshold": Elem.INT(""),
-        "p14enwg": Elem.BOOL(""),
-        "p14enwg_input": Elem.INT(""),
-        "p14enwg_type": Elem.INT(""),
+        "extended_logging": Elem.BOOL("Gibt an, ob weiterführende Informationen zu den Steuerentscheidungen im Ereignis-Log aufgezeichnet werden sollen."),
+        "yield_forecast": Elem.BOOL("Gibt an, ob die PV-Ertragsprognose für die Heizungssteuerung verwendet werden soll."),
+        "yield_forecast_threshold": Elem.INT("Grenzwert Für die PV-Ertragsprognose. Wenn die PV-Ertragsprognosen-Steuerung aktiviert ist und die Prognose größer als die eingestellte Energie ausfällt, entfällt die Dynamische-Strom­preis-Steuerung und es wird ausschließlich der PV-Über­schuss genutzt.", unit=Units.kWh),
+        "extended": Elem.BOOL("Gibt an, ob die Steuerung des erweiterten Betriebs aktiviert werden soll."),
+        "extended_hours": Elem.INT("Anzahl der (günstigsten) Stunden für die der erweiterte Betrieb genutzt werden soll.", unit=Units.h),
+        "blocking": Elem.BOOL("Gibt an, ob die Steuerung des blockierenden Betriebs aktiviert werden soll."),
+        "blocking_hours": Elem.INT("Anzahl der (teuersten) Stunden für die der blockierende Betrieb genutzt werden soll.", unit=Units.h),
+        "pv_excess_control": Elem.BOOL("Gibt an, ob die PV-Überschusssteuerung aktiviert werden soll."),
+        "pv_excess_control_threshold": Elem.INT("Grenzewert für die PV-Überschusssteuerung. Wenn die PV-Überschusssteuerung aktiviert ist und der PV-Überschuss größer als der eingestellte Wert ausfällt, wird die Heizung nur bei PV-Überschuss in den erweiterten Betrieb gebracht.", unit=Units.W),
+        "p14enwg": Elem.BOOL("Gibt an, ob die §14a EnWG-Steuerung aktiviert werden soll."),
+        "p14enwg_input": Elem.INT("Gibt an, welcher Eingang des Energiemanagers für die Abschaltung per §14a EnWG genutzt werden soll.", constants=[
+            Const(0, "Eingang 1"),
+            Const(1, "Eingang 2"),
+            Const(1, "Eingang 3"),
+            Const(1, "Eingang 4"),
+        ]),
+        "p14enwg_type": Elem.INT("Konfiguration für den Eingang.", constants=[
+            Const(0, "Aktiv wenn geschlossen"),
+            Const(1, "Aktiv wenn geöffnet"),
+        ])
     })),
 
     Func("state", FuncType.STATE, Elem.OBJECT("Der Zustand der Heizungssteuerung.", members={
-        "sgr_blocking": Elem.BOOL(""),
-        "sgr_extended": Elem.BOOL(""),
-        "p14enwg": Elem.BOOL(""),
-        "next_update": Elem.INT(""),
+        "sgr_blocking": Elem.BOOL("Gibt an, ob der blockierende Betrieb aktiv ist."),
+        "sgr_extended": Elem.BOOL("Gibt an, ob der erweiterte Betrieb aktiv ist."),
+        "p14enwg": Elem.BOOL("Gibt an, ob blockierender Betrieb auf Grund von §14a EnWG aktiv ist."),
+        "next_update": Elem.INT("Ein Unix-Timestamp in <strong>Minuten</strong>, der angibt wann die Heizungssteuerung die nächste Aktualisierung durchführt."),
     })),
 ])
