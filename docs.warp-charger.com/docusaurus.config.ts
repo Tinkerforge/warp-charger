b79ef8491c627062d22cac17e33fbe77edfc6e9b
Olaf Lüke <olaf@tinkerforge.com>
2026-01-13 21:03:55 +0100

    Move onBrokenMarkdownLinks configuration around to avoid deprecation warning

--- docs.warp-charger.com/docusaurus.config.ts
+++ docs.warp-charger.com/docusaurus.config.ts
@@ -19,7 +19,6 @@ const config: Config = {
   projectName: 'warp-charger', // Usually your repo name.
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
@@ -168,6 +167,9 @@ const config: Config = {
 
   markdown: {
     mermaid: true,
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+    },
   },
   themes: ['@docusaurus/theme-mermaid'],
 };
