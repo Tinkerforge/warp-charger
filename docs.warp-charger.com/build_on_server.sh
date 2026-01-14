da7d2e0872c403f9f8a295cca8ed16b355c8220a
Olaf Lüke <olaf@tinkerforge.com>
2026-01-14 19:16:43 +0100

    docs.warp-charger.com: Improve build on server script

--- docs.warp-charger.com/build_on_server.sh
+++ docs.warp-charger.com/build_on_server.sh
@@ -1,8 +1,29 @@
 #!/bin/sh
-
 # Simple auto-deploy from github
 # This is called on docs.warp-charger.com with cron
-cd /var/www/git/warp-charger/docs.warp-charger.com/
+
+set -e
+
+REPO_DIR="/var/www/git/warp-charger/docs.warp-charger.com"
+BRANCH="master"
+
+cd "$REPO_DIR"
+
+# Fetch updates
+/usr/bin/git fetch origin "$BRANCH"
+
+LOCAL_HASH=$(/usr/bin/git rev-parse HEAD)
+REMOTE_HASH=$(/usr/bin/git rev-parse "origin/$BRANCH")
+
+# Exit silently if nothing changed
+[ "$LOCAL_HASH" = "$REMOTE_HASH" ] && exit 0
+
+# Update working tree
+/usr/bin/git reset --hard "origin/$BRANCH"
+
+# Build only when changes were pulled
 /usr/local/bin/npm ci
 /usr/local/bin/npm run build
+
+# Deploy build output
 cp -r build/* build_release/
