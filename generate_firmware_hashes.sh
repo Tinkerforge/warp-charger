bc7852d18432805844da2d9323dafcfccbc517b6
Matthias Bolte <matthias@tinkerforge.com>
2026-09-15 10:00:26 +0200

    firmwares: Make checksum errors fatal and ignore latest symlinks

--- generate_firmware_hashes.sh
+++ generate_firmware_hashes.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash -e
 cd firmwares
 for file in *.sha256; do sha256sum -c $file; done
-for file in *.bin; do if [ ! -f $file.sha256 ]; then sha256sum $file > $file.sha256; fi done
+for file in *.bin; do if [[ "$file" != *"_latest_"* ]] && [ ! -f "$file.sha256" ]; then sha256sum "$file" > "$file.sha256"; fi; done
